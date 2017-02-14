bbeb23617c24abe39c84521ab3ff93947508cc21
Simon Winter <simon.winter@infontology.org>
2017-02-14 09:40:06 +0100

    Förbättrad kod som bör fixa hela databashanteringen

--- db_test.py
+++ db_test.py
@@ -25,72 +25,40 @@ def hello_world():
 
     curs = conn.cursor()
 
-    fields = list(('Livsmedelsnamn text', 'Livsmedelsnummer text', 'Energi_kcal real', 'Energi_kJ real', 'Kolhydrater_g real', 'Fett_g real', 'Protein_g real', 'Fibrer_g real', 'Vatten_g real', 'Alkohol_g real', 'Aska_g real', 'Monosackarider_g real', 'Disackarider_g real', 'Sackaros_g real', 'Fullkorn_totalt_g real', 'Sockerarter_g real', 'Summa_mattade_fettsyror_g real', 'Fettsyra_40-100_g real', 'Fettsyra_120_g real', 'Fettsyra_140_g real', 'Fettsyra_160_g real', 'Fettsyra_180_g real', 'Fettsyra_200_g real', 'Summa_enkelomattade_fettsyror_g real', 'Fettsyra_161_g real', 'Fettsyra_181_g real', 'Summa_fleromattade_fettsyror_g real', 'Fettsyra_182_g real', 'Fettsyra_183_g real', 'Fettsyra_204_g real', 'EPA_Fettsyra_205_g real', 'DPA_Fettsyra_225_g real', 'DHA_Fettsyra_226_g real', 'Kolesterol_mg real', 'Retinol_mikrog real', 'Vitamin_A_mikrog real', 'beta-Karoten_mikrog real', 'Vitamin_D_mikrog real', 'Vitamin_E_mg real', 'Vitamin_K_mikrog real', 'Tiamin_mg real', 'Riboflavin_mg real', 'Vitamin_C_mg real', 'Niacin_mg real', 'Niacinekvivalenter_mg real', 'Vitamin_B6_mg real', 'Vitamin_B12_mikrog real', 'Folat_mikrog real', 'Fosfor_mg real', 'Jod_mikrog real', 'Jarn_mg real', 'Kalcium_mg real', 'Kalium_mg real', 'Magnesium_mg real', 'Natrium_mg real', 'Salt_g real', 'Selen_mikrog real', 'Zink_mg real', 'Avfall_skal_etc_ real'))
+    fields = str(('Livsmedelsnamn text', 'Livsmedelsnummer text', 'Energi_kcal real', 'Energi_kJ real', 'Kolhydrater_g real', 'Fett_g real', 'Protein_g real', 'Fibrer_g real', 'Vatten_g real', 'Alkohol_g real', 'Aska_g real', 'Monosackarider_g real', 'Disackarider_g real', 'Sackaros_g real', 'Fullkorn_totalt_g real', 'Sockerarter_g real', 'Summa_mattade_fettsyror_g real', 'Fettsyra_40-100_g real', 'Fettsyra_120_g real', 'Fettsyra_140_g real', 'Fettsyra_160_g real', 'Fettsyra_180_g real', 'Fettsyra_200_g real', 'Summa_enkelomattade_fettsyror_g real', 'Fettsyra_161_g real', 'Fettsyra_181_g real', 'Summa_fleromattade_fettsyror_g real', 'Fettsyra_182_g real', 'Fettsyra_183_g real', 'Fettsyra_204_g real', 'EPA_Fettsyra_205_g real', 'DPA_Fettsyra_225_g real', 'DHA_Fettsyra_226_g real', 'Kolesterol_mg real', 'Retinol_mikrog real', 'Vitamin_A_mikrog real', 'beta-Karoten_mikrog real', 'Vitamin_D_mikrog real', 'Vitamin_E_mg real', 'Vitamin_K_mikrog real', 'Tiamin_mg real', 'Riboflavin_mg real', 'Vitamin_C_mg real', 'Niacin_mg real', 'Niacinekvivalenter_mg real', 'Vitamin_B6_mg real', 'Vitamin_B12_mikrog real', 'Folat_mikrog real', 'Fosfor_mg real', 'Jod_mikrog real', 'Jarn_mg real', 'Kalcium_mg real', 'Kalium_mg real', 'Magnesium_mg real', 'Natrium_mg real', 'Salt_g real', 'Selen_mikrog real', 'Zink_mg real', 'Avfall_skal_etc_ real'))
+    fields = fields.strip('()')
 
-    #string_test = str(['Livsmedelsnamn text', 'Livsmedelsnummer text', 'Energi_kcal real']).strip('[]')
-    string_test = str(fields).strip('[]')
+    #print fields
 
-    string_test = string_test.decode()
+    sql_create = '''CREATE TABLE food2 (%s)''' % fields
 
-    sql_create = '''CREATE TABLE food2 (%s)''' % string_test
-    #sql_create = '''CREATE TABLE food2 ('Livsmedelsnamn text', 'Livsmedelsnummer text', 'Energi_kcal real')'''
-
-    print sql_create
-    print 'här!'
+    #print sql_create
 
     curs.execute('''DROP TABLE IF EXISTS food2''')
     curs.execute(sql_create)
 
-    #with io.open('/Users/organization/PycharmProjects/foodcluster/LivsmedelsDB_201611160847_01_liten.csv', encoding="utf-8") as csvfile:
-    #    d = [tuple(line) for line in csv.reader(csvfile)]
+    def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
+        csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
+        for row in csv_reader:
+            yield [unicode(cell, 'utf-8') for cell in row]
+    data = []
+    filename = '/Users/organization/PycharmProjects/foodcluster/LivsmedelsDB_201611160847_01_liten.csv'
+    reader = unicode_csv_reader(open(filename))
+    for field1 in reader:
+        data.append(tuple(field1))
 
-    d=[]
-    fields = tuple(d)
+    reader.close()
 
-    #d=d[1]
-
-    #('Vitt br\xc3\xb6d fibrer 3,5% ospec', '202', '265.6', '1111.4', '47.7', '3.61', '8.17', '3.5', '35.5', '0', '1.54', '1.96', '2.36', '0.05', '0', '', '0.43', '0', '0.01', '0.01', '0.25', '0.11', '0.01', '1.2', '0.01', '1.15', '0.77', '0.62', '0.14', '0', '0', '0', '0', '0', '17.7', '18.2', '6', '0', '0.88', '3.5', '0.22', '0.09', '0', '2.14', '3.5', '0.19', '0.14', '37.1', '141.7', '2', '1.12', '43.9', '173.8', '29.4', '400', '1', '4.4', '0.75', '0')
-
-    mystring = 'u'
-
-
-    print mystring
-    print 'torsdag'
-
-
-    print d
-    print 'här!'
-
-    e = (
-        (u'Fått', 44, 34.5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),
-        (u'Fisk', 37, 23.5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)
-    )
-
-    d=[(u'Vitt br\xf6d fibrer 3,5% ospec', u'202', u'265.6', u'1111.4', u'47.7', u'3.61', u'8.17', u'3.5', u'35.5', u'0', u'1.54', u'1.96', u'2.36', u'0.05', u'0', u'', u'0.43', u'0', u'0.01', u'0.01', u'0.25', u'0.11', u'0.01', u'1.2', u'0.01', u'1.15', u'0.77', u'0.62', u'0.14', u'0', u'0', u'0', u'0', u'0', u'17.7', u'18.2', u'6', u'0', u'0.88', u'3.5', u'0.22', u'0.09', u'0', u'2.14', u'3.5', u'0.19', u'0.14', u'37.1', u'141.7', u'2', u'1.12', u'43.9', u'173.8', u'29.4', u'400', u'1', u'4.4', u'0.75', u'0')]
-
-    curs.executemany("INSERT INTO food2 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", d)
-    # curs.execute("INSERT INTO food VALUES ('Gurka ','45',454,0.5,'40')")
+    #Sätt in data i databasen
+    curs.executemany("INSERT INTO food2 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
 
     # Commit the inserted rows.
     conn.commit()
-
-    hurManga = '?'
-    for x in range(len(fields) - 1):
-        hurManga += ', ?'
-
-    print str(len(hurManga)) + ' antal platshållare'
-
-    #curs.executemany("INSERT INTO food2 VALUES (%s)" % hurManga, d)
-
-    #conn.commit()
 
     # Now fetch back the inserted data and write it to JSON.
     curs.execute("SELECT * FROM food2 order by 'Livsmedelsnummer' ")
     recs = curs.fetchall()
 
-    print type(recs)
-    print 'typ'
-    print 'torsdag'
     print "DB data as a list with a dict per DB record:"
     rows = [dict(rec) for rec in recs]
     print rows
@@ -101,39 +69,15 @@ def hello_world():
     print 'Här blir det en tuple:'
     for row in curs.execute('select * from food2'):
         print row
-        result=result.append(row)
+        result+=row
+
 
     print 'result'
-    print result
+    print tuple(result)
 
     print 'slut på det'
 
-    # Create table.
-    curs.execute('''DROP TABLE IF EXISTS food''')
-    curs.execute('''CREATE TABLE food (name text, number int, fat real)''')
 
-    # Insert a few rows of data.
-    e = (
-        ('Dressing', 44, 34.5),
-        ('Groda', 37, 23.5)
-    )
-
-    curs.executemany("INSERT INTO food VALUES (?, ?, ?)", e)
-    # curs.execute("INSERT INTO food VALUES ('Gurka ','45',454,0.5,'40')")
-
-    # Commit the inserted rows.
-    conn.commit()
-
-    # Now fetch back the inserted data and write it to JSON.
-    curs.execute("SELECT * FROM food")
-    recs = curs.fetchall()
-
-    print type(recs)
-    print 'typ'
-
-    print "DB data as a list with a dict per DB record:"
-    rows = [dict(rec) for rec in recs]
-    print rows
 
     # We can also close the connection if we are done with it.
     conn.close()
@@ -234,15 +178,14 @@ def unicodeimport ():
         csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
         for row in csv_reader:
             yield [unicode(cell, 'utf-8') for cell in row]
-
+    data = []
     filename = '/Users/organization/PycharmProjects/foodcluster/LivsmedelsDB_201611160847_01_liten.csv'
     reader = unicode_csv_reader(open(filename))
     for field1 in reader:
-        print tuple(field1)
+        data.append(tuple(field1))
 
-    f=[]
-    with codecs.open('/Users/organization/PycharmProjects/foodcluster/LivsmedelsDB_201611160847_01_liten.csv', encoding='utf-8'):
-        for field1 in f:
-            print tuple(field1)
+    reader.close()
+
+    print (data)
 
     return (u'Hej då')
