d128c6ae1c803e01940c4074132a358b531690a7
Simon Winter <simon.winter@infontology.org>
2017-09-22 09:46:21 +0200

    Funkar tillräckligt bra...

--- scrape-google-image-search.py
+++ scrape-google-image-search.py
@@ -1,5 +1,3 @@
-# Baserat på denna: https://gist.github.com/kekeblom/204a609ee295c81c3cc202ecbe68752c
-
 # Copyright (c) 2016 Kenneth Blomqvist
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -31,7 +29,7 @@ from selenium import webdriver
 
 argument_parser = argparse.ArgumentParser(description='Download images using google image search')
 argument_parser.add_argument('query', metavar='query', type=str, help='The query to download images from')
-argument_parser.add_argument('--count', metavar='count', default=5, type=int, help='How many images to fetch')
+argument_parser.add_argument('--count', metavar='count', default=100, type=int, help='How many images to fetch')
 argument_parser.add_argument('--label', metavar='label', type=str, help="The directory in which to store the images (images/<label>)", required=True)
 
 
@@ -57,57 +55,36 @@ def fetch_image_urls(query, images_to_download):
     search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
     browser = webdriver.Firefox()
     browser.get(search_url.format(q=query))
-    #def scroll_to_bottom():
-    #    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    #    time.sleep(2)
+    def scroll_to_bottom():
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
 
-    image_count = 0
+    image_count = len(image_urls)
     delta = 0
-    #while image_count < images_to_download:
-    print("Found:", len(image_urls), "images")
-    #scroll_to_bottom()
+    while image_count < images_to_download:
+        print("Found:", len(image_urls), "images")
+        scroll_to_bottom()
 
-    images = browser.find_elements_by_css_selector("img.rg_ic")
-    #for img in images:
-    #    image_count =+ 1
-    image_urls.add(images[0].get_attribute('src'))
-    #delta = len(image_urls) - image_count
-    #image_count = len(image_urls)
-    #break
-    #if delta == 0:
-    #    print("Can't find more images")
-    #    break
+        images = browser.find_elements_by_css_selector("img.rg_ic")
+        for img in images:
+            image_urls.add(img.get_attribute('src'))
+        delta = len(image_urls) - image_count
+        image_count = len(image_urls)
 
-    #fetch_more_button = browser.find_element_by_css_selector(".ksb._kvc")
-    #if fetch_more_button:
-    #    browser.execute_script("document.querySelector('.ksb._kvc').click();")
-    #    scroll_to_bottom()
+        if delta == 0:
+            print("Can't find more images")
+            break
+
+        fetch_more_button = browser.find_element_by_css_selector(".ksb._kvc")
+        if fetch_more_button:
+            browser.execute_script("document.querySelector('.ksb._kvc').click();")
+            scroll_to_bottom()
 
     browser.quit()
-    return image_urls
+    #print(image_urls)
+    image_urls=list(image_urls)
+    return image_urls[0]
 
-def persist_image(dir_image_src):
-    label_directory = dir_image_src[0]
-    image_src = dir_image_src[1]
-
-    size = (256, 256)
-    try:
-        image_content = requests.get(image_src).content
-    except requests.exceptions.InvalidSchema:
-        # image is probably base64 encoded
-        image_data = re.sub('^data:image/.+;base64,', '', image_src)
-        image_content = base64.b64decode(image_data)
-    except Exception as e:
-        print("could not read", e, image_src)
-        return False
-
-    image_file = io.BytesIO(image_content)
-    image = Image.open(image_file).convert('RGB')
-    #resized = image.resize(size)
-    with open('./images/' + args.label + '_' + args.query + ".jpg", 'wb')  as f:
-        image.save(f, "JPEG", quality=85)
-        image.close()
-    return True
 
 
 if __name__ == '__main__':
@@ -115,15 +92,34 @@ if __name__ == '__main__':
 
     ensure_directory('./images/')
 
-    query_directory = './images/' + args.label + "/"
-    ensure_directory(query_directory)
+    query_directory = './images/'
+    #ensure_directory(query_directory)
 
-    image_urls = fetch_image_urls(args.query, args.count)
-
-    values = [item for item in zip(itertools.cycle([query_directory]), image_urls)]
+    image_url = fetch_image_urls(args.query, args.count)
+    values=[query_directory,image_url]
+    #values = [item for item in zip(itertools.cycle([query_directory]), image_url)]
     #print (values)
-    print("image count", len(image_urls))
 
-    pool = Pool(12)
-    results = pool.map(persist_image, values)
-    print("Images downloaded: ", len([r for r in results if r]))
+    try:
+        image_content = requests.get(image_url).content
+    except requests.exceptions.InvalidSchema:
+        # image is probably base64 encoded
+        image_data = re.sub('^data:image/.+;base64,', '', image_url)
+        image_content = base64.b64decode(image_data)
+    except Exception as e:
+        print("could not read", e, image_src)
+        #return False
+
+
+
+    #print (image_content)
+    #print("image count", len(image_urls))
+    image_file = io.BytesIO(image_content)
+    image = Image.open(image_file).convert('RGB')
+    #resized = image.resize(size)
+    with open(query_directory + args.label + '_' + args.query + ".jpg", 'wb')  as f:
+        image.save(f, "JPEG", quality=85)
+
+    #pool = Pool(12)
+    #results = pool.map(persist_image, values)
+    #print("Images downloaded: ", len([r for r in results if r]))
